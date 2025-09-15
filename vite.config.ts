7c74480a586c39c15450b37eabcbe2230282e6fa
ali-a-devx <110853127+ali-a-devx@users.noreply.github.com>
2025-09-15 12:19:14 +0300

    Update index.html, package.json, and Vite configuration; change document type, title, and add meta tags for SEO. Switch from BrowserRouter to HashRouter for routing in App component, and adjust homepage URL in package.json.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/portfolio',
+  base: "/portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
